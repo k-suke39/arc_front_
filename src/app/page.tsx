cb7c741001b2179fd5254b9b566b7af8f1eea6a4
k-suke39 <pz.mozarla@gmail.com>
2023-09-07 15:09:49 +0900

    ⚡️ add: footerのコンポーネント化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { getAllTodos } from '@/api';
 import Header from './components/Header';
 import LoginButton from './components/LoginButton';
 import TimeLine from './components/TimeLine';
+import Footer from './components/Footer';
 
 export default async function Home() {
 	return (
@@ -15,21 +16,7 @@ export default async function Home() {
 			</main>
 			<LoginButton />
 			<TimeLine />
-			<footer className="px-4 sm:px-6 lg:px-8">
-				<div className="relative mx-auto max-w-[37.5rem] pt-20 text-center pb-20">
-					<div className="flex items-center justify-center space-x-5">
-						<p className="mt-2 font-bold text-base leading-7 text-slate-600 ">
-							利用規約
-						</p>
-						<p className="mt-2 font-bold text-base leading-7 text-slate-600 ">
-							プライバシーポリシー
-						</p>
-						<p className="mt-2 font-bold text-base leading-7 text-slate-600 ">
-							お問い合わせ
-						</p>
-					</div>
-				</div>
-			</footer>
+			<Footer />
 		</div>
 	);
 }
