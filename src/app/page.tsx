8c22d8b5d9e6a52509544a7856ac721879395582
k-suke39 <pz.mozarla@gmail.com>
2023-09-07 14:15:18 +0900

    🔥 mod: lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image';
+import axios from 'axios';
+import { getAllTodos } from '@/api';
+
 export default async function Home() {
 	return (
 		<div>
@@ -36,6 +40,7 @@ export default async function Home() {
 								className="border-2-2 absolute border-opacity-20 border-gray-700 h-full border"
 								style={{ left: '50%' }}
 							></div>
+
 							<div className="mb-8 flex justify-between items-center w-full right-timeline">
 								<div className="order-1 w-5/12"></div>
 								<div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
@@ -62,6 +67,7 @@ export default async function Home() {
 									/>
 								</div>
 							</div>
+
 							<div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
 								<div className="order-1 w-5/12"></div>
 								<div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
@@ -88,6 +94,7 @@ export default async function Home() {
 									/>
 								</div>
 							</div>
+
 							<div className="mb-8 flex justify-between items-center w-full right-timeline">
 								<div className="order-1 w-5/12"></div>
 								<div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
