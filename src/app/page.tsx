9be3c966d02267836a959622ad9d716d91ba2b90
k-suke39 <pz.mozarla@gmail.com>
2023-09-07 15:08:46 +0900

    ⚡️ add: timelineのコンポーネント化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { getAllTodos } from '@/api';
 import Header from './components/Header';
 import LoginButton from './components/LoginButton';
+import TimeLine from './components/TimeLine';
 
 export default async function Home() {
 	return (
@@ -13,109 +14,7 @@ export default async function Home() {
 				</section>
 			</main>
 			<LoginButton />
-			<section className="relative mx-auto max-w-5xl  sm:px-6  overflow-hidden">
-				<article className="md:flex">
-					<div className="container mx-auto w-full h-full">
-						<div className="relative wrap overflow-hidden h-full">
-							<div
-								className="border-2-2 absolute border-opacity-20 border-gray-700 h-full border"
-								style={{ left: '50%' }}
-							></div>
-
-							<div className="mb-8 flex justify-between items-center w-full right-timeline">
-								<div className="order-1 w-5/12"></div>
-								<div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-									<h1 className="mx-auto font-semibold text-lg text-white">
-										1
-									</h1>
-								</div>
-								<div className="order-1 bg-gray-200 rounded-lg shadow-xl w-5/12 px-6 py-4">
-									<h3 className="mb-3 font-bold text-gray-700 text-xl">
-										問題集を選択
-									</h3>
-									<p className="text-sm leading-snug tracking-wide text-gray-700 text-opacity-100">
-										hogehogehoge
-									</p>
-									<img
-										loading="lazy"
-										src="/logo.png"
-										alt="Logspot"
-										className="h-20 w-20"
-									/>
-								</div>
-							</div>
-
-							<div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-								<div className="order-1 w-5/12"></div>
-								<div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-									<h1 className="mx-auto text-white font-semibold text-lg">
-										2
-									</h1>
-								</div>
-								<div className="order-1 bg-gray-200 rounded-lg shadow-xl w-5/12 px-6 py-4">
-									<h3 className="mb-3 font-bold text-gray-700 text-xl">
-										チャレンジしたい問題を選択
-									</h3>
-									<p className="text-sm font-medium leading-snug tracking-wide text-gray-700 text-opacity-100">
-										hogehogehoge
-									</p>
-									<img
-										loading="lazy"
-										src="/logo.png"
-										alt="Logspot"
-										className="h-20 w-20"
-									/>
-								</div>
-							</div>
-
-							<div className="mb-8 flex justify-between items-center w-full right-timeline">
-								<div className="order-1 w-5/12"></div>
-								<div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-									<h1 className="mx-auto font-semibold text-lg text-white">
-										3
-									</h1>
-								</div>
-								<div className="order-1 bg-gray-200 rounded-lg shadow-xl w-5/12 px-6 py-4">
-									<h3 className="mb-3 font-bold text-gray-800 text-xl">
-										エディタにコードを書く
-									</h3>
-									<p className="text-sm leading-snug tracking-wide text-gray-900 text-opacity-100">
-										hogehogehoge
-									</p>
-									<img
-										loading="lazy"
-										src="/logo.png"
-										alt="Logspot"
-										className="h-20 w-20"
-									/>
-								</div>
-							</div>
-							<div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-								<div className="order-1 w-5/12"></div>
-								<div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-									<h1 className="mx-auto text-white font-semibold text-lg">
-										4
-									</h1>
-								</div>
-								<div className="order-1 bg-gray-200 rounded-lg shadow-xl w-5/12 px-6 py-4">
-									<h3 className="mb-3 font-bold text-gray-700 text-xl">
-										自分で書いたコードを判定する
-									</h3>
-									<p className="text-sm font-medium leading-snug tracking-wide text-gray-700 text-opacity-100">
-										hogehogehoge
-									</p>
-									<img
-										loading="lazy"
-										src="/logo.png"
-										alt="Logspot"
-										className="h-20 w-20"
-									/>
-								</div>
-							</div>
-						</div>
-					</div>
-				</article>
-			</section>
+			<TimeLine />
 			<footer className="px-4 sm:px-6 lg:px-8">
 				<div className="relative mx-auto max-w-[37.5rem] pt-20 text-center pb-20">
 					<div className="flex items-center justify-center space-x-5">
