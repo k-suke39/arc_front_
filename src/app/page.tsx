df1fe3ca566eaae11bd43cfcad861dabefa451b5
k-suke39 <pz.mozarla@gmail.com>
2023-09-07 14:12:37 +0900

    🔥 mod: lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,6 @@ export default async function Home() {
 								className="border-2-2 absolute border-opacity-20 border-gray-700 h-full border"
 								style={{ left: '50%' }}
 							></div>
-
 							<div className="mb-8 flex justify-between items-center w-full right-timeline">
 								<div className="order-1 w-5/12"></div>
 								<div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
@@ -63,7 +62,6 @@ export default async function Home() {
 									/>
 								</div>
 							</div>
-
 							<div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
 								<div className="order-1 w-5/12"></div>
 								<div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
@@ -90,7 +88,6 @@ export default async function Home() {
 									/>
 								</div>
 							</div>
-
 							<div className="mb-8 flex justify-between items-center w-full right-timeline">
 								<div className="order-1 w-5/12"></div>
 								<div className="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
